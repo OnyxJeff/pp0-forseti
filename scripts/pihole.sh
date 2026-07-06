aa622cf5c84faa41ab2976ea97da0b3b402a754a
OnyxJeff <j.may2702@gmail.com>
2026-07-05 20:05:12 -0400

    modified for ansible

--- scripts/pihole.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# pihole.sh
-# Script to deploy Pi-hole via its automated installer
-
-set -euo pipefail
-
-echo "Starting deployment of Pi-hole..."
-curl -sSL https://install.pi-hole.net | bash
-echo "Pi-hole deployment finished."
